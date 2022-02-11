c75ac4a8a1f3ca76ccf47e9154c912695769737e
Valentina Unzhakova <vunzhakova@outlook.com>
2022-02-11 22:14:21 +0200

    1задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,64 +1,39 @@
-
 import { galleryItems } from "./gallery-items.js";
 
-// Change code below this line
+console.log(galleryItems);
 
-const galleryContainer = document.querySelector(".gallery");
-const galleryItemMarkup = createImageGalleryMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", galleryItemMarkup);
+const galleryItemsEl = document.querySelector(".gallery");
 
-galleryContainer.addEventListener("click", onImageClick);
-
-function createImageGalleryMarkup(galleryItems) {
-  return galleryItems
+const selectorGallery = (images) => {
+  return images
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-                  <a class="gallery__link" href="${original}"">
-                  <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}""
-                    alt="${description}""
-                  />
-                </a>
-              </div> `;
+      return `<div class="gallery__item"><a class="gallery__link" href=${original}><img class= "gallery__image" src="${preview}" data-source =${original} alt="${description}"></div>`;
     })
     .join("");
-    
-}
-function onImageClick(event) {
+};
+
+const cardImagesMarkup = selectorGallery(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("afterbegin", cardImagesMarkup);
+
+galleryItemsEl.addEventListener("click", selectGalleryEl);
+
+function selectGalleryEl(event) {
   event.preventDefault();
-  const isGalleryItem = event.target.classList.contains("gallery__image");
-  if (!isGalleryItem) {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
-  document.addEventListener("keydown", closeModal);
-
-  const modalWindow = basicLightbox.create(
-    `
-    <div class="modal">
-    <img src="${event.target.dataset.source}" width ="800" height = "600">
-    </div>
-    `,
-    {
-      onShow: (modalWindow) => {
-        console.log("onShow", modalWindow);
-      },
-      onClose: (modalWindow) => {
-        document.removeEventListener("keydown", closeModal);
-        console.log("onClose", modalWindow);
-      },
-    }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
   );
-  showModal();
-  closeModal(event);
+  instance.show();
 
-  function showModal() {
-    modalWindow.show();
-  }
-  function closeModal(event) {
+  const onKeydownEsc = (event) => {
+    console.log(event.code);
     if (event.code === "Escape") {
-      modalWindow.close();
+      instance.close();
     }
-  }
+  };
+
+  window.addEventListener("keydown", onKeydownEsc);
 }
