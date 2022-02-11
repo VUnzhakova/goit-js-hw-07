ceb457e0b0ebfdd84983335a65b55e3d25fb8cc2
Valentina Unzhakova <vunzhakova@outlook.com>
2022-02-11 22:15:34 +0200

    2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,39 +1,29 @@
-
 import { galleryItems } from './gallery-items.js';
-
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
+console.log(galleryItems);
 
-galleryContainer.insertAdjacentHTML(
-  "beforeend",
-  createImageGalleryMarkup(galleryItems)
-);
+const galleryItemsEl = document.querySelector('.gallery');
 
-function createImageGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}"">
-  <img
-    
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}""
-    alt="${description}""
-  />
-</a>
-</li> `;
-    })
-    .join("");
+const selectorGallery = (images) => {
+    return images.map(({ preview, original, description }) => {
+        return `<li class="gallery__list"><a class="gallery__item" href=${original}><img class= "gallery__image" src="${preview}" alt="" title="${description}"></a></li>`
+    }).join('');
 }
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captions: true,
-  captionDelay: 250,
-  captionSelector: "img",
-  captionType: "attr",
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionClass: "",
-});
+const cardImagesMarkup = selectorGallery(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("afterbegin", cardImagesMarkup);
+
+galleryItemsEl.addEventListener('click', selectGalleryEl);
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+function selectGalleryEl(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    lightbox;
+}
